6a4514b1463ba93b1ae1cbc67de8cec3bb51737a
Resha Puspita <reshapuspita734@gmail.com>
2020-07-21 14:37:59 +0700

    add user page

--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { List, ListItemText } from "@material-ui/core";
+import { ListAltRounded } from "@material-ui/icons";
+
+import TableComponent from "../../Components/TableComponent/TableComponent";
+import ModalEdit from "../../Components/ModalEdit/ModalEdit";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+
 function UserPage() {
-  return <div></div>;
+  const modalEdit = <ModalEdit />;
+  const tabTasks = (
+    <List>
+      <ListAltRounded /> <ListItemText primary="Tasks" />
+    </List>
+  );
+  const table = <TableComponent modalEdit={modalEdit} />;
+
+  return (
+    <div>
+      <Sidebar tabTasks={tabTasks} table={table} />
+    </div>
+  );
 }
 
 export default UserPage;
