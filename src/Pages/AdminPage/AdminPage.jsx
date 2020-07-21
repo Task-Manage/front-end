889c2ad88f85d117dc11eff82af7e842942ca8eb
Resha Puspita <reshapuspita734@gmail.com>
2020-07-21 14:37:52 +0700

    add admin page

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { List, ListItemText } from "@material-ui/core";
+import { AssignmentIndRounded, ListAltRounded } from "@material-ui/icons";
+
 import TableComponent from "../../Components/TableComponent/TableComponent";
 import AlertDelete from "../../Components/AlertDelete/AlertDelete";
 import ModalEdit from "../../Components/ModalEdit/ModalEdit";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 
 function AdminPage() {
   const alertDelete = <AlertDelete />;
   const modalEdit = <ModalEdit />;
+  const table = (
+    <TableComponent modalEdit={modalEdit} alertDelete={alertDelete} />
+  );
+  const tabTasks = (
+    <List>
+      <ListAltRounded /> <ListItemText primary="Tasks" />
+    </List>
+  );
+  const tabEmployees = (
+    <List>
+      <AssignmentIndRounded /> <ListItemText primary="Employees" />
+    </List>
+  );
   return (
     <div>
-      <TableComponent modalEdit={modalEdit} alertDelete={alertDelete} />
+      <Sidebar tabEmployees={tabEmployees} tabTasks={tabTasks} table={table} />
     </div>
   );
 }
