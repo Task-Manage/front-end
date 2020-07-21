2a13200d652bbb4eb34457a474ecb665fd15ba17
jujulili1 <tyulianto@gmail.com>
2020-07-21 23:16:24 +0700

    functionstyle login fix

--- src/Pages/Login/LoginAdmin.jsx
+++ src/Pages/Login/LoginAdmin.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,6 +13,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -36,7 +38,42 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn() {
   const classes = useStyles();
+  let history = useHistory();
 
+  const [userData, setUserData] = useState({
+      email: '',
+      password: '',
+  });
+
+  function handleChange(event) {
+      setUserData({
+          ...userData,
+          [event.target.name]: event.target.value,
+      });
+  }
+
+  async function handleSubmit(event) {
+      event.preventDefault();
+
+      const options = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+      };
+
+      const response = await fetch(
+          'https://task-manage-adm.herokuapp.com/api/users/login',
+          options
+      );
+      const result = await response.json();
+
+      localStorage.setItem('user', result.token);
+if (result.role === "admin" ) 
+{history.push('/adminPage');}
+else {history.push('/userPage');}
+  }
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -57,6 +94,8 @@ export default function SignIn() {
             label="Email Address"
             name="email"
             autoComplete="email"
+            onChange={handleChange}
+            value={userData.email}
             autoFocus
           />
           <TextField
@@ -68,6 +107,8 @@ export default function SignIn() {
             label="Password"
             type="password"
             id="password"
+            onChange={handleChange}
+            value={userData.password}
             autoComplete="current-password"
           />
           <FormControlLabel
@@ -79,6 +120,7 @@ export default function SignIn() {
             fullWidth
             variant="contained"
             color="primary"
+            onClick={handleSubmit}
             className={classes.submit}
           >
             Sign In
