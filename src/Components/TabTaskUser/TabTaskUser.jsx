59ff231e6215c41f6d578e56be2ec9352e5accdd
agusTrih <ant3dong@gmail.com>
2020-07-22 22:14:57 +0700

    delete nav link in tabtask user and userpage

--- src/Components/TabTaskUser/TabTaskUser.jsx
+++ src/Components/TabTaskUser/TabTaskUser.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+
 import { List, ListItemText, ListItem, ListItemIcon } from '@material-ui/core';
 import { ListAltRounded } from '@material-ui/icons';
 
--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import TableTaskUser from '../../Components/TableTaskUser/TableTaskUser';
 import ModalEditUser from '../../Components/ModalEditUser/ModalEditUser';
 import Sidebar from '../../Components/Sidebar/Sidebar';
-import TabTask from '../../Components/TabTask/TabTask';
+
 import TabTaskUser from '../../Components/TabTaskUser/TabTaskUser';
 
 function UserPage() {
