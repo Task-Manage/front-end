a7da9c4d19e610145ec3794c60dfcd558df2b924
Resha Puspita <reshapuspita734@gmail.com>
2020-07-22 19:59:26 +0700

    delete task and employee by admin

--- src/Components/AlertDelete/AlertDelete.jsx
+++ src/Components/AlertDelete/AlertDelete.jsx
@@ -1,48 +1,66 @@
-import React from "react";
+import React from 'react';
 
-import { Button, Dialog, DialogTitle, DialogActions } from "@material-ui/core";
-import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import { Button, Dialog, DialogTitle, DialogActions } from '@material-ui/core';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
 function AlertDelete(props) {
-  const [openDelete, setOpenDelete] = React.useState(false);
-  const id = props.id;
-  const handleClickOpenDelete = () => {
-    setOpenDelete(true);
-  };
+    const [openDelete, setOpenDelete] = React.useState(false);
+    const id = props.id;
+    const url = `${props.url}/${id}`;
+    const handleClickOpenDelete = () => {
+        setOpenDelete(true);
+    };
 
-  const handleCloseDelete = () => {
-    setOpenDelete(false);
-  };
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    };
 
-  return (
-    <div>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={handleClickOpenDelete}
-      >
-        <DeleteOutlineIcon /> Delete
-      </Button>
-      <Dialog
-        open={openDelete}
-        onClose={handleCloseDelete}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Are You Sure Want to Delete?"}
-        </DialogTitle>
-        <DialogActions>
-          <Button onClick={handleCloseDelete} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleCloseDelete} color="primary" autoFocus>
-            Yes, Delete It
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+    const handleDelete = () => {
+        const token = JSON.parse(localStorage.getItem('user')).token;
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${token}`,
+            },
+        };
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((results) => {
+                setOpenDelete(false);
+                window.location.reload(true);
+            });
+    };
+
+    return (
+        <div>
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleClickOpenDelete}
+            >
+                <DeleteOutlineIcon /> Delete
+            </Button>
+            <Dialog
+                open={openDelete}
+                onClose={handleCloseDelete}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {'Are You Sure Want to Delete?'}
+                </DialogTitle>
+                <DialogActions>
+                    <Button onClick={handleCloseDelete} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDelete} color="primary" autoFocus>
+                        Yes, Delete It
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
 
 export default AlertDelete;
