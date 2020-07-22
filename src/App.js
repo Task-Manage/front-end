765d8ca817132d7a804a5299d35758624cb97452
Resha Puspita <reshapuspita734@gmail.com>
2020-07-22 14:17:09 +0700

    tambahin private route admin task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    // Redirect,
-} from 'react-router-dom';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Redirect,
+} from "react-router-dom";
 
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-import PrivateRoute from './Helpers/PrivateRoute';
+import PrivateRoute from "./Helpers/PrivateRoute";
 
 import AlertDelete from "./Components/AlertDelete/AlertDelete";
 import ModalAdd from "./Components/ModalAdd/ModalAdd";
@@ -37,19 +37,19 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/'>
-             <Login />
+          <Route exact path="/">
+            <Login />
           </Route>
-          <Route exact path='/register'>
-             <Register />
+          <Route exact path="/register">
+            <Register />
           </Route>
-          <Route exact path="/admin/tasks">
+          <PrivateRoute exact path="/admin/tasks">
             <AdminPage
               table={tableTaskAdmin}
               addButton={addButton}
               pageTitle={"Task List"}
             />
-          </Route>
+          </PrivateRoute>
           {/* <Redirect from="/admin" to="/admin/employees" /> */}
           <PrivateRoute exact path="/admin/employees">
             <AdminPage table={tableEmployeeAdmin} pageTitle={"Employee List"} />
