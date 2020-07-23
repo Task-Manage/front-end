338bfecb0828fa98d7f7f3c0ed182d049b208cc9
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-07-23 11:58:58 +0700

    Fix table pagination in employee table of admin page

--- src/Components/TableEmployeeAdmin/TableEmployeeAdmin.jsx
+++ src/Components/TableEmployeeAdmin/TableEmployeeAdmin.jsx
@@ -1,3 +1,138 @@
+// import React, { useState, useEffect } from 'react';
+// import { withStyles, makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+// import TableHead from '@material-ui/core/TableHead';
+// import TablePagination from '@material-ui/core/TablePagination';
+// import TableRow from '@material-ui/core/TableRow';
+
+// // import ButtonGroup from "@material-ui/core/ButtonGroup";
+// import AlertDelete from '../AlertDelete/AlertDelete';
+
+// const StyledTableCell = withStyles((theme) => ({
+//     head: {
+//         backgroundColor: theme.palette.info.dark,
+//         color: theme.palette.common.white,
+//     },
+//     body: {
+//         fontSize: 14,
+//     },
+// }))(TableCell);
+
+// const useStyles = makeStyles({
+//     root: {
+//         width: '100%',
+//     },
+//     container: {
+//         maxHeight: 440,
+//     },
+// });
+
+// export default function TableEmployeeAdmin(props) {
+//     const classes = useStyles();
+//     const [page, setPage] = useState(0);
+//     const [rowsPerPage, setRowsPerPage] = useState(10);
+//     const [employeeData, setEmployeeData] = useState(null);
+//     const urlDelete = `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users`;
+//     const url = `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/getAllUserAdminPage`;
+
+//     const handleChangePage = (event, newPage) => {
+//         setPage(newPage);
+//     };
+
+//     const handleChangeRowsPerPage = (event) => {
+//         setRowsPerPage(+event.target.value);
+//         setPage(0);
+//     };
+
+//     useEffect(() => {
+//         const token = JSON.parse(localStorage.getItem('user')).token;
+//         const options = {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 authorization: `Bearer ${token}`,
+//             },
+//         };
+
+//         fetch(url, options)
+//             .then((response) => response.json())
+//             .then((results) => setEmployeeData(results.result));
+//     }, [url]);
+
+//     const rows = [];
+
+//     return (
+//         <Paper className={classes.root}>
+//             <TableContainer className={classes.container}>
+//                 <Table stickyHeader aria-label="sticky table">
+//                     <TableHead>
+//                         <TableRow>
+//                             <StyledTableCell style={{ minWidth: '170' }}>
+//                                 ID
+//                             </StyledTableCell>
+//                             <StyledTableCell style={{ minWidth: '100' }}>
+//                                 Name
+//                             </StyledTableCell>
+//                             <StyledTableCell style={{ minWidth: '170' }}>
+//                                 Email
+//                             </StyledTableCell>
+//                             <StyledTableCell
+//                                 style={{ minWidth: '180' }}
+//                             ></StyledTableCell>
+//                         </TableRow>
+//                     </TableHead>
+//                     <TableBody>
+//                         {employeeData !== null &&
+//                             employeeData
+//                                 .slice(
+//                                     page * rowsPerPage,
+//                                     page * rowsPerPage + rowsPerPage
+//                                 )
+//                                 .map((employee) => {
+//                                     return (
+//                                         <TableRow
+//                                             hover
+//                                             role="checkbox"
+//                                             tabIndex={-1}
+//                                         >
+//                                             <TableCell>
+//                                                 {employee._id}
+//                                             </TableCell>
+//                                             <TableCell>
+//                                                 {employee.name}
+//                                             </TableCell>
+//                                             <TableCell>
+//                                                 {employee.email}
+//                                             </TableCell>
+//                                             <AlertDelete
+//                                                 id={employee._id}
+//                                                 url={urlDelete}
+//                                             />
+//                                         </TableRow>
+//                                     );
+//                                 })}
+//                     </TableBody>
+//                 </Table>
+//             </TableContainer>
+//             <TablePagination
+//                 rowsPerPageOptions={[10, 25, 100]}
+//                 component="div"
+//                 count={rows.length}
+//                 rowsPerPage={rowsPerPage}
+//                 page={page}
+//                 onChangePage={handleChangePage}
+//                 onChangeRowsPerPage={handleChangeRowsPerPage}
+//             />
+//         </Paper>
+//     );
+// }
+
+// ==========================================================
+
 import React, { useState, useEffect } from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
@@ -39,6 +174,24 @@ export default function TableEmployeeAdmin(props) {
     const urlDelete = `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users`;
     const url = `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/getAllUserAdminPage`;
 
+    const columns = [
+        { id: 'id', label: 'ID', minWidth: 170 },
+        { id: 'name', label: 'Name', minWidth: 100 },
+
+        {
+            id: 'email',
+            label: 'Email',
+            minWidth: 170,
+            align: 'right',
+        },
+        {
+            id: 'options',
+            label: 'Options',
+            minWidth: 170,
+            align: 'right',
+        },
+    ];
+
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
@@ -60,11 +213,14 @@ export default function TableEmployeeAdmin(props) {
 
         fetch(url, options)
             .then((response) => response.json())
-            .then((results) => setEmployeeData(results.result));
-    }, [url]);
+            .then((results) => {
+                setEmployeeData(results.result);
+            });
+
+        // eslint-disable-next-line
+    }, []);
 
     console.log(employeeData);
-    const rows = [];
 
     return (
         <Paper className={classes.root}>
@@ -72,18 +228,14 @@ export default function TableEmployeeAdmin(props) {
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
                         <TableRow>
-                            <StyledTableCell style={{ minWidth: '170' }}>
-                                ID
-                            </StyledTableCell>
-                            <StyledTableCell style={{ minWidth: '100' }}>
-                                Name
-                            </StyledTableCell>
-                            <StyledTableCell style={{ minWidth: '170' }}>
-                                Email
-                            </StyledTableCell>
-                            <StyledTableCell
-                                style={{ minWidth: '180' }}
-                            ></StyledTableCell>
+                            {columns.map((column) => (
+                                <StyledTableCell
+                                    key={column.id}
+                                    style={{ minWidth: '170' }}
+                                >
+                                    {column.label}
+                                </StyledTableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -122,7 +274,7 @@ export default function TableEmployeeAdmin(props) {
             <TablePagination
                 rowsPerPageOptions={[10, 25, 100]}
                 component="div"
-                count={rows.length}
+                count={employeeData !== null && employeeData.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onChangePage={handleChangePage}
